4ba48ce1277d3edc43617d0c7e7528f3424cf3c2
Navneesh Yadav <yadavnavneesh999@gmail.com>
2022-11-02 13:09:13 +0530

    Hosting in heroku

---
+++ public/js/main.js
@@ -0,0 +1,101 @@
+const cityName = document.getElementById('cityName');
+const submitBtn = document.getElementById('submitBtn');
+
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+const datahide = document.querySelector('.middle_layer');
+const day=document.getElementById("day");
+const today_date=document.getElementById("today_date");
+
+const getInfo = async(event) => {
+    event.preventDefault();
+
+    let cityVal = cityName.value;
+
+    if(cityVal === ""){
+        city_name.innerText = `Plz write the name before search`;
+        datahide.classList.add("data_hide");
+    }else{
+
+        try{
+           
+
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=cd899f115b39ec5027a36bb91c368dc0`
+            const response = await fetch(url);
+
+            const data = await response.json();
+            const arrData = [data];
+
+            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+            temp_real_val.innerText = arrData[0].main.temp;
+            const tempMood = arrData[0].weather[0].main;
+            console.log(tempMood);
+
+            //condition to check sunny or cloudy
+            if (tempMood == "Clear") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
+            } else if (tempMood == "Clouds") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
+            } else if (tempMood == "Rain") {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
+            } else {
+            temp_status.innerHTML =
+                "<i class='fas  fa-cloud' style='color:#f1f2f6;'></i>";
+
+            }
+            datahide.classList.remove('data_hide');
+            cityVal = "";
+           
+       
+        }catch{
+            cityVal = " ";
+            datahide.classList.add("data_hide");
+            city_name.innerText =  `please enter the proper city name`;
+            console.log('please add the proper city name');
+        }
+        
+    }
+}
+
+const getCurrentDay = () => {
+    let weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    let currentTime = new Date();
+    let day = weekday[currentTime.getDay()];
+    return day;
+  };
+  const getCurrentTime = () => {
+    let months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "June",
+      "July",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    let  now = new Date();
+    let month = months[now.getMonth() + 1];
+    let date = now.getDate();
+    return `${date} ${month}`;
+  }
+  day.innerHTML=getCurrentDay();
+  today_date.innerHTML=getCurrentTime();
+
+submitBtn.addEventListener('click', getInfo);
